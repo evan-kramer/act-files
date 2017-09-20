07b941c72d834a334f0fd47ec747612757d5fdea
evan-kramer <evan.kramer@tn.gov>
2017-09-20 09:25:41 -0500

    Missing SAS-matched file?

--- act_highest_score_file_2017.R
+++ act_highest_score_file_2017.R
@@ -7,3 +7,15 @@ library(lubridate)
 library(haven)
 library(stringr)
 
+# Read in data
+grad = read_csv("K:/ORP_accountability/data/2017_graduation_rate/student_level_20170830.csv")
+act_highest = read_dta("K:/Assessment_Data Returns/ACT/2016-17/Graduating Class- Highest/20170914_ACT_GraduatingCohortHighestScore_SY2016-17_Whalen_v1.dta")
+act_state_day = read_dta("K:/ORP_accountability/data/2016_ACT/TN_790Select_2016.dta")
+
+# Start with graduates
+student_level = left_join(mutate(student_key = as.double(student_key)), act_highest, 
+                          by = c("student_key" = "unique_student_id"))
+
+# Append SAS-matched records - DON'T CURRENTLY HAVE THESE?!
+
+
