2e017082574433aa19c11293c549366f99fb3744
Evan Kramer <evan.kramer@tn.gov>
2017-09-26 10:14:44 -0500

    includes matching for missing IDs (Tae)

--- act_highest_score_file_2017.R
+++ act_highest_score_file_2017.R
@@ -9,12 +9,13 @@ library(stringr)
 
 # Markers
 date = str_c(day(today()), month(today(), label = T), year(today()))
-sys = T
-sch = T 
+sys = F
+sch = F 
 
 # Read in data
 grad = read_csv("K:/ORP_accountability/data/2017_graduation_rate/student_level_20170830.csv")
 act_highest = read_dta("K:/Assessment_Data Returns/ACT/2016-17/Graduating Class- Highest/20170914_ACT_GraduatingCohortHighestScore_SY2016-17_Whalen_v1.dta")
+act_tae = read_dta("K:/Assessment_Data Returns/ACT/2016-17/Graduating Class- Highest/20170921_ACT_GraduatingCohortHighestScore_SY2016-17_Tae_matching.dta")
 act_state_day2016 = read_dta("K:/ORP_accountability/data/2016_ACT/TN_790Select_2016.dta")
 act_state_day2017 = read_dta("K:/Assessment_Data Returns/ACT/2016-17/Junior Day File/20170713_ACT_JuniorDayResults_SY2016-17_Whalen_v1.dta")
 
@@ -30,6 +31,12 @@ act = transmute(act_highest, unique_student_id, test_date = act_testdate, englis
     bind_rows(transmute(act_state_day2017, unique_student_id = state_stud_id, test_date = testdate,
                         english = act_eng, math = act_math, reading = act_read, science = act_sci, 
                         composite = act_composite, file = "State day 2017")) %>% 
+    bind_rows(act_tae %>% 
+                  filter(is.na(unique_student_id) | usid != unique_student_id) %>% 
+                  transmute(unique_student_id = usid, test_date = act_testdate, 
+                            english = act_english_highest, math = act_math_highest, 
+                            reading = act_reading_highest, science = act_science_highest, 
+                            composite = act_composite_highest, file = "Tae match")) %>% 
     mutate(test_date = ifelse(test_date > 999, str_c(str_sub(as.character(test_date), 1, 2), "0120", str_sub(as.character(test_date), 3, 4)), 
                               str_c("0", str_sub(test_date, 1, 1), "0120", str_sub(as.character(test_date), 2, 3))),
            test_date = mdy(test_date),
@@ -38,7 +45,7 @@ act = transmute(act_highest, unique_student_id, test_date = act_testdate, englis
             desc(english), desc(science), desc(test_date), file) %>% 
     group_by(unique_student_id) %>% 
     summarize_all(funs(first(.)))
-    
+
 # Include only graduates
 student_level = grad %>% 
     filter(included_in_cohort == "Y" & completion_type == 1) %>% 
